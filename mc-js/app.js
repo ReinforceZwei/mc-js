5b5bc1003f06af3e970915226534fff2eab479ee
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2020-03-30 22:56:23 +0800

    Add another cli method

--- mc-js/app.js
+++ mc-js/app.js
@@ -1,16 +1,29 @@
 'use strict';
 
 var fs = require("fs");
+//var readline = require('readline');
 var mc = require("minecraft-protocol");
 var EventHandler = require("./EventHandler");
 var CommandHandler = require("./CommandHandler");
 
 var config = require("./config");
 
+// CLI method 1 - Using stdin
 var stdin = process.openStdin();
 stdin.addListener("data", function (d) {
     CommandHandler.OnCommand(d.toString().trim());
 });
+/* CLI method 2 - Using readline
+var rl = readline.createInterface(process.stdin, process.stdout);
+rl.setPrompt('> ');
+rl.prompt();
+rl.on('line', function (d) {
+    console.log("Input: " + d);
+    rl.setPrompt('> ');
+    rl.prompt();
+    //CommandHandler.OnCommand(d.toString().trim());
+});
+*/
 
 var client = mc.createClient(config);
 
